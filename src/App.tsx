0ba42201de9c66b87a004c4aeb9ae8f5b3dc4876
RageNeko26 <rneko2006@gmail.com>
2023-06-30 12:38:32 +0700

    feat: added new component into main

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Toggler from './components/Toggler'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,9 +24,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button>
-          Display Title
-        </button>
+        <Toggler />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
